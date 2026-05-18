f396da2278e16ba5d52e64a74730110bd5e9595b
jeevana-s <siruvellajeevanajyothi@gmail.com>
2026-05-18 15:13:35 +0530

    Delete backend/src/main/java/com/linkforge/config/KafkaTopics.java

--- backend/src/main/java/com/linkforge/config/KafkaTopics.java
+++
@@ -1,8 +0,0 @@
-package com.linkforge.config;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.context.annotation.*;
-@Configuration
-public class KafkaTopics {
-  public static final String CLICKS = "linkforge.clicks";
-  @Bean NewTopic clicks(){ return new NewTopic(CLICKS, 3, (short)1); }
-}
